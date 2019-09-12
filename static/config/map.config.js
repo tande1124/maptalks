2a0b12d278f90304393bbda85946992c422ade60
lecooer <tande1124@163.com>
2019-09-12 14:46:54 +0800

    矢量、地形、影像

--- static/config/map.config.js
+++ static/config/map.config.js
@@ -1,7 +1,13 @@
+/*
+ * @Descripttion:
+ * @Date: 2019-09-11 09:41:34
+ * @LastEditors: tande
+ * @LastEditTime: 2019-09-12 14:39:04
+ */
 (function () {
   /**
- * 地图（天地图）配置文件
- */
+     * 地图（天地图）配置文件
+     */
   var mapOptions = {
     zoom: 9,
     minZoom: 3,
@@ -29,126 +35,132 @@
       imperial: false
     }
   }
-  var baseLayers = [
-    {
-      id: 'vec_c',
-      layerType: 'TileLayer',
-      tileSystem: [1, -1, -180, 90],
-      urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=vec_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
-      subdomains: ['1', '2', '3', '4', '5'],
-      visible: true,
-      crossOrigin: 'anonymous'
-    },
-    {
-      id: 'cva_c',
-      layerType: 'TileLayer',
-      tileSystem: [1, -1, -180, 90],
-      urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=cva_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
-      subdomains: ['1', '2', '3', '4', '5'],
-      visible: true,
-      crossOrigin: 'anonymous'
-    },
-    {
-      id: 'img_c',
-      layerType: 'TileLayer',
-      tileSystem: [1, -1, -180, 90],
-      urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=img_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
-      subdomains: ['1', '2', '3', '4', '5'],
-      visible: false,
-      crossOrigin: 'anonymous'
-    },
-    {
-      id: 'cia_c',
-      layerType: 'TileLayer',
-      tileSystem: [1, -1, -180, 90],
-      urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=cia_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
-      subdomains: ['1', '2', '3', '4', '5'],
-      visible: false,
-      crossOrigin: 'anonymous'
-    },
-    {
-      id: 'ter_c',
-      layerType: 'TileLayer',
-      tileSystem: [1, -1, -180, 90],
-      urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=ter_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
-      subdomains: ['1', '2', '3', '4', '5'],
-      visible: false,
-      crossOrigin: 'anonymous'
-    },
-    {
-      id: 'cta_c',
-      layerType: 'TileLayer',
-      tileSystem: [1, -1, -180, 90],
-      urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=cta_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
-      subdomains: ['1', '2', '3', '4', '5'],
-      visible: false,
-      crossOrigin: 'anonymous'
-    }
+  var baseLayers = [{ // 矢量
+    id: 'vec_c',
+    layerType: 'TileLayer',
+    tileSystem: [1, -1, -180, 90],
+    urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=vec_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
+    subdomains: ['1', '2', '3', '4', '5'],
+    visible: true,
+    crossOrigin: 'anonymous'
+  },
+  {
+    id: 'cva_c',
+    layerType: 'TileLayer',
+    tileSystem: [1, -1, -180, 90],
+    urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=cva_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
+    subdomains: ['1', '2', '3', '4', '5'],
+    visible: true,
+    crossOrigin: 'anonymous'
+  },
+  { // 影像
+    id: 'img_c',
+    layerType: 'TileLayer',
+    tileSystem: [1, -1, -180, 90],
+    urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=img_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
+    subdomains: ['1', '2', '3', '4', '5'],
+    visible: false,
+    crossOrigin: 'anonymous'
+  },
+  {
+    id: 'cia_c',
+    layerType: 'TileLayer',
+    tileSystem: [1, -1, -180, 90],
+    urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=cia_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
+    subdomains: ['1', '2', '3', '4', '5'],
+    visible: false,
+    crossOrigin: 'anonymous'
+  },
+  { // 地形
+    id: 'ter_c',
+    layerType: 'TileLayer',
+    tileSystem: [1, -1, -180, 90],
+    urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=ter_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
+    subdomains: ['1', '2', '3', '4', '5'],
+    visible: false,
+    crossOrigin: 'anonymous'
+  },
+  {
+    id: 'cta_c',
+    layerType: 'TileLayer',
+    tileSystem: [1, -1, -180, 90],
+    urlTemplate: 'http://t{s}.tianditu.gov.cn/DataServer?T=cta_c&tk=4f62e1d82bd46e2ff470b291c2260156&x={x}&y={y}&l={z}',
+    subdomains: ['1', '2', '3', '4', '5'],
+    visible: false,
+    crossOrigin: 'anonymous'
+  }
   ]
 
   // ----------------------通用部分----------------------------
-  var layers = [
-    {
-      id: 'label_layer',
-      layerType: 'VectorLayer'
-    },
-    {
-      id: 'plot_layer',
-      layerType: 'VectorLayer'
-    },
-    {
-      id: 'buffer_layer',
-      layerType: 'VectorLayer'
-    },
-    {
-      id: 'heat_layer',
-      layerType: 'HeatLayer',
-      radius: 80,
-      blur: 30,
-      gradient: { 0.4: 'blue', 0.65: 'lime', 1: 'red' }
-    },
-    {
-      id: 'cluster_layer',
-      layerType: 'ClusterLayer',
-      noClusterWithOneMarker: true,
-      maxClusterZoom: 16,
-      symbol: {
-        markerType: 'ellipse',
-        markerFill: {
-          property: 'count',
-          type: 'interval',
-          stops: [
-            [0, 'rgb(0, 115, 206)'],
-            [9, 'rgb(0, 115, 206)'],
-            [99, 'rgb(0, 115, 206)']
-          ]
-        },
-        markerFillOpacity: 0.7,
-        markerLineOpacity: 1,
-        markerLineWidth: 1,
-        markerLineColor: '#fff',
-        markerWidth: {
-          property: 'count',
-          type: 'interval',
-          stops: [[0, 40], [9, 50], [99, 60]]
-        },
-        markerHeight: {
-          property: 'count',
-          type: 'interval',
-          stops: [[0, 40], [9, 50], [99, 60]]
-        }
+  var layers = [{
+    id: 'label_layer',
+    layerType: 'VectorLayer'
+  },
+  {
+    id: 'plot_layer',
+    layerType: 'VectorLayer'
+  },
+  {
+    id: 'buffer_layer',
+    layerType: 'VectorLayer'
+  },
+  {
+    id: 'heat_layer',
+    layerType: 'HeatLayer',
+    radius: 80,
+    blur: 30,
+    gradient: { 0.4: 'blue', 0.65: 'lime', 1: 'red' }
+  },
+  {
+    id: 'cluster_layer',
+    layerType: 'ClusterLayer',
+    noClusterWithOneMarker: true,
+    maxClusterZoom: 16,
+    symbol: {
+      markerType: 'ellipse',
+      markerFill: {
+        property: 'count',
+        type: 'interval',
+        stops: [
+          [0, 'rgb(0, 115, 206)'],
+          [9, 'rgb(0, 115, 206)'],
+          [99, 'rgb(0, 115, 206)']
+        ]
       },
-      textSymbol: {
-        'textFill': '#fff',
-        'textFaceName': '"microsoft yahei"',
-        'textSize': 16,
-        'textDx': 0,
-        'textDy': 4
+      markerFillOpacity: 0.7,
+      markerLineOpacity: 1,
+      markerLineWidth: 1,
+      markerLineColor: '#fff',
+      markerWidth: {
+        property: 'count',
+        type: 'interval',
+        stops: [
+          [0, 40],
+          [9, 50],
+          [99, 60]
+        ]
       },
-      drawClusterText: true,
-      geometryEvents: true,
-      single: true
-    }
+      markerHeight: {
+        property: 'count',
+        type: 'interval',
+        stops: [
+          [0, 40],
+          [9, 50],
+          [99, 60]
+        ]
+      }
+    },
+    textSymbol: {
+      'textFill': '#fff',
+      'textFaceName': '"microsoft yahei"',
+      'textSize': 16,
+      'textDx': 0,
+      'textDy': 4
+    },
+    drawClusterText: true,
+    geometryEvents: true,
+    single: true
+  }
   ]
   var services = {
     baseServiceUrl: 'http://106.37.227.20:8081/ntwcf'
